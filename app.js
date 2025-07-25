318527217e90c4f3ed03dc7f80d800779ca50aa0
root <root@DESKTOP-RU9OJA3.localdomain>
2025-07-25 18:02:24 +0000

    connected User route to register post

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express();
 const userRouter = require('./routes/user.routes');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs');
 app.use('/user', userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,17 +4,12 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.send('User Registered')
+});
 
 
 
-// /*  http://localhost:3000/user/test   */
-
-// router.get('/test', (req, res) =>{
-//     res.send("hello user this is test route")
-// })
-// /* http://localhost:3000/user/index */
-// router.get('/index', (req, res) =>{
-//     res.render('new_index');
-// })
 
 module.exports = router;
