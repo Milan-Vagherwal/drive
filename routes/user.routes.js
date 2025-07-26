87d5cf4cd28453b65cef3710daafc10a721948a4
root <root@DESKTOP-RU9OJA3.localdomain>
2025-07-26 05:46:20 +0000

    auth error handling

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,14 +7,17 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', 
-    body('email').trim().isEmail(),
+    body('email').trim().isEmail().isLength({min:13}),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3}),
     (req, res) => {
         const errors = validationResult(req);
         
         if(!errors.isEmpty()) {
-            res.send("invalid Data")
+            return res.status(400).json({
+                errors:errors.array(),
+                message: 'Invalid data'
+            })
         }
         res.send(errors)
 });
